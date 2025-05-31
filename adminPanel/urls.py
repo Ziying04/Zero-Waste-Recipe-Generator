b5a073322fbf8850368944e3f2f1ed7bd4254439
WT-26 <musnowt.wt@gmail.com>
2025-06-01 01:16:08 +0800

    Update routing and logic in urls.py and views.py

--- adminPanel/urls.py
+++ adminPanel/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import admin_dashboard
+from .views import admin_dashboard, admin_user
 
 urlpatterns = [
-    path('admin/dashboard/', admin_dashboard, name='admin_dashboard'),
+    path('admin/', admin_dashboard, name='admin_dashboard'),
+    path('admin/users/', admin_user, name='admin_user'),
+    # Add other paths as needed
 ]
+
--- adminPanel/views.py
+++ adminPanel/views.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 
-# Create your views here.
+def admin_dashboard(request):
+    # Count the number of users
+    total_users = User.objects.count()
+    ## context = {
+    ##    "total_users": total_users,
+    ## } 
+
+    print(f"Total Users: {total_users}")  # Check the console for the total user count
+    
+    return render(request, "adminPage.html", {'total_users': total_users})
+
+def admin_user(request):
+    users = User.objects.all()
+    total_users = users.count()
+    return render(request, "adminUser.html", {"users": users, "total_users": total_users})
