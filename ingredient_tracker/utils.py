32ed8d782b2709e129ad2bc80188fefd9e2dc148
Ziying04 <mandyezy@gmail.com>
2025-06-09 01:46:35 +0800

    fix the ai generator (use available ingredient)

--- ingredient_tracker/utils.py
+++ ingredient_tracker/utils.py
@@ -33,3 +33,16 @@ def get_user_ingredients(user):
         return status_order.get(ingredient.status, 99)
 
     return sorted(ingredients, key=sort_key)
+
+
+def filter_ingredients(ingredients, filter_type):
+    if filter_type == "all":
+        return ingredients
+    elif filter_type == "expiring_soon":
+        return [i for i in ingredients if i.status == "Expiring Soon"]
+    elif filter_type == "expired":
+        return [i for i in ingredients if i.status == "Expired"]
+    elif filter_type == "fresh":
+        return [i for i in ingredients if i.status == "Fresh"]
+    else:
+        return []
